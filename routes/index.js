fa08d586099a88f406e8c41235396dc2673dca54
A.S.Sahasranamam <Spock-II@ASSahasranamams-MacBook-Pro-2.local>
2017-07-05 14:27:36 +0530

    with grid view

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('104.154.188.76:27017/test');
+var test =  mongoose.connect('104.154.188.76:27017/test');
+//var formDatadb = mongoose.connect('104.154.188.76:27017/formData');
 var Schema = mongoose.Schema;
-var id = mongoose.Types.ObjectId();
+//var id = mongoose.Types.ObjectId();
 var ObjectID = require('mongodb').ObjectID;
 var http = require('http');
+var GenerateSchema = require('generate-schema');
 
 var request = require('request');
 var iditem;
@@ -15,14 +17,14 @@ var infomdata;
 var userDataSchema = new Schema({
     _id: Schema.Types.ObjectId,
     script: String,
+    ngmod: [String],
+    que: [String]
 }, {
     collection: 'userData'
 });
 
-var UserData = mongoose.model('UserData', userDataSchema);
-
-
-
+var UserData = test.model('UserData', userDataSchema);
+console.log("test.type" + typeof userDataSchema);
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -37,19 +39,13 @@ router.get('/test', function (req, res, next) {
 });
 
 router.get('/display', function (req, res, next) {
+     //  test = mongoose.connect('104.154.188.76:27017/test');
+
     res.render('display', {
         title: 'Express'
     });
 
     console.log("/display queryid" + req.query._id);
-    UserData.find({
-        _id: ObjectID(req.query._id)
-
-    }, function (err, docs) {
-        console.log("display: " + docs);
-        infomdata = docs;
-        // res.send(docs);
-    });
 
 
 });
@@ -68,8 +64,10 @@ router.get('/display', function (req, res, next) {
 // res.send(docs);
 //});
 var copyofid = "";
-
+var ngarray = [];
 router.post('/insert', function (req, res, next) {
+  //  test =mongoose.connect('104.154.188.76:27017/test');
+
     // var i = JSON.stringify(req);
     //  console.log("$scope.questionelemnt :"+i);
     // console.log("req : "+req)
@@ -77,45 +75,60 @@ router.post('/insert', function (req, res, next) {
     console.log("BODY: " + req.body);
     var body = req.body;
     var script = "";
-    var script2 = '<input type="text" placeholder = "sample"/>';
+    ngarray = [];
+    que = [];
+    ///var script2 = '<input type="text" placeholder = "sample"/>';
+    //SCRIPT GEN BELOW
     for (var i = 0; i < body.length; i++) {
         switch (body[i].type) {
             case 'text':
-              
-                var addon =  '<div class=list-group-item><span><label>' + body[i].question + '</label> : <input type =text placeholder=' + body[i].placeholder + ' ng-model='+ body[i].type + body[i].id  +'  > </span> </div>' + ' {{'+body[i].type + body[i].id+'}}';
+
+                var addon = '<div class=list-group-item><span><label>' + body[i].question + '</label> : <input type =text placeholder=' + body[i].placeholder + ' ng-model=' + body[i].type + body[i].id + ' id=gen > </span> </div>' + ' {{' + body[i].type + body[i].id + '}}';
+                ngarray.push(body[i].type + body[i].id);
+                que.push(body[i].question);
                 script = script + addon;
                 break;
 
             case 'checkbox':
                 var duplicator = "";
-                for (var j = 0; j < body[i].content.length;j++) {
-                    duplicator = duplicator + '<span><label>' + body[i].content[j] + '</label> : <input type=checkbox ng-model=' + body[i].id + j + ' ng-true-value=true ng-false-value=false></br>'
+                for (var j = 0; j < body[i].content.length; j++) {
+                    duplicator = duplicator + '<span><label>' + body[i].content[j] + '</label> : <input type=checkbox ng-model=' + body[i].type + body[i].id + j + ' ng-checked=false></br>';
+                    ngarray.push(body[i].type + body[i].id + j);
+                    que.push(body[i].question + '_' + body[i].content[j]);
                 }
-                var addon = ' <div class=list-group-item><span><label>' + body[i].question + ' </br>' + duplicator +' </span> </div>';
+                var addon = ' <div class=list-group-item><span><label>' + body[i].question + ' </br>' + duplicator + ' </span> </div>';
                 script = script + addon;
                 break;
 
             case 'radio':
                 var duplicator = "";
-                for (var j = 0; j < body[i].content.length;j++)  {
-                    duplicator = duplicator + '<span><label>' + body[i].content[j] + '</label> : <input type=radio ng-model='+ body[i].type+ body[i].id+' name ='+ body[i].id +' ></br>'
+                for (var j = 0; j < body[i].content.length; j++) {
+                    duplicator = duplicator + '<span><label>' + body[i].content[j] + '</label> : <input type=radio ng-model=' + body[i].type + body[i].id + ' name =' + body[i].id + ' value= ' + body[i].content[j] + '></br>';
+                    console.log("radio :  " + body[i].content[j]);
+
+
                 }
-                var addon = ' <div class=list-group-item><span><label>' + body[i].question + '</br>' + duplicator + '</span> </div>';
+                var addon = ' <div class=list-group-item><span><label>' + body[i].question + '</br>' + duplicator + '</span> </div> ' + ' {{' + body[i].type + body[i].id + '}}';
+                que.push(body[i].question);
+                ngarray.push(body[i].type + body[i].id);
                 script = script + addon;
                 break;
 
             case 'button':
-                var addon = ' <div class=list-group-item"><span><label>' + body[i].question + '</label> : <button   ng-model='+ body[i].question  + ' >' + body[i].buttontext  + '</button></span></div>    ';
+                var addon = ' <div class=list-group-item"><span><label>' + body[i].type + body[i].id + '</label> : <button   ng-model=' + body[i].question + ' >' + body[i].buttontext + '</button></span></div> ';
                 script = script + addon;
                 break;
         }
         console.log(script);
+        console.log("que : " + que);
     }
 
     var item = {
         _id: new ObjectID(),
-      //  data: req.body,
-        script: script
+        //  data: req.body,
+        script: script,
+        que: que,
+        ngmod: ngarray
         // content: req.body.content,
         // author: req.body.author
     };
@@ -162,11 +175,64 @@ router.post('/insert2', function (req, res, next) {
 
 var smapleid;
 
-router.get('/infom', function (req, res, next) {
+router.post('/infom', function (req, res, next) {
+    console.log("post /infom func");
+    console.log(req.body.query);
+    UserData.find({
+        _id: ObjectID(req.body.query)
 
-    console.log("infomdata :" + infomdata);
-    res.send(infomdata);
+    }, 'script -_id ', function (err, docs) {
+        console.log("display: " + docs);
+        //  infomdata=docs;
+        //if (err){ throw err};
+        res.send(docs);
+        //infomdata=infomdata.slice(12);
+        //console.log(infomdata);
+    });
+
+    //    console.log("infomdata :" + docs);
+    //res.send(infomdata);
 });
+
+router.post('/ngarr', function (req, res, next) {
+
+    console.log("post /ngarr func");
+    console.log(req.body.query);
+    UserData.find({
+        _id: ObjectID(req.body.query)
+
+    }, 'ngmod -_id ', function (err, docs) {
+        console.log("ngmod: " + docs);
+        //  infomdata=docs;
+        //if (err){ throw err};
+        res.send(docs);
+        //infomdata=infomdata.slice(12);
+        //console.log(infomdata);
+    });
+});
+
+router.post('/que', function (req, res, next) {
+
+    console.log("post /que func");
+    console.log(req.body.query);
+    UserData.find({
+        _id: ObjectID(req.body.query)
+
+    }, 'que -_id ', function (err, docs) {
+        console.log("que: " + docs);
+        //  infomdata=docs;
+        //if (err){ throw err};
+        res.send(docs);
+        //infomdata=infomdata.slice(12);
+        //console.log(infomdata);
+    });
+});
+
+
+var datatypes = []; //strips numbers off ngmod
+var quest = []; //stores questions
+var schematypes = []; //storesdatatyoes
+var userinput; //stores userinput
 
 
 router.get('/get-info', function (req, next, res) {
@@ -188,5 +254,55 @@ router.get('/get-info', function (req, next, res) {
     console.log(data);
     res.send(docs);
 });
+
+var FormData;
+var formDataSchema;
+
+router.post('/insertCli', function (req, res, next) {
+    delete mongoose.models['FormData'];
+
+    
+//    var test = mongoose.connect('104.154.188.76:27017/test');
+    var query = req.body.query;
+    console.log(query);
+    //var jsonArrSch = [] ;
+    var jsonStringSch = req.body.jsonStringSch;
+    console.log(jsonStringSch);
+    var jsonStringSchp = req.body.jsonStringSchp;
+    //  jsonStringSchp=JSON.parse(jsonStringSchp);
+    //  console.log(jsonStringSchp);
+    var jsonStringAns = req.body.jsonStringAns;
+    //  jsonStringAns=JSON.parse(jsonStringAns);
+    console.log(jsonStringAns);
+    //console.log(err);
+    // var jsonArrSch0=JSON.parse(jsonArrSch[0]);
+    //console.log(jsonArrSch0);
+
+    console.log("inside func");
+    formDataSchema = GenerateSchema.mongoose(jsonStringAns);
+    //new Schema(jsonStringSch, {strict:false});
+
+    console.log("schema done");
+    console.log(formDataSchema);
+    //  console.log( typeof Data );
+    //   console.log(userDataSchema);
+    console.log("before");
+    query = "tbl" + query;
+    console.log(query);
+    FormData = test.model('FormData', formDataSchema, query);
+    console.log(typeof FormData);
+    console.log("-------------------");
+    console.log(FormData);
+    var formData = new FormData(jsonStringAns);
+    FormData.create(formData);
+    //test.disconnect();
+    res.send("helo");
+});
+
+
+
+
+
+//var FormData= new mongoose.model();
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+//mongoose.connect('104.154.188.76:27017/test');
+//var Schema = mongoose.Schema;
+//var id = mongoose.Types.ObjectId();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://104.154.188.76:27017/test";
+var ObjectID = require('mongodb').ObjectID;
+var http = require('http');
+var GenerateSchema = require('generate-schema');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.render('result');
+});
+
+router.post('/fetch', function (req, res, next) {
+    console.log("inside users/fetch")
+    var q = req.body.query;
+    q = "tbl" + q;
+    console.log(q);
+    var data;
+
+    MongoClient.connect(url, function (err, db) {
+        //assert.equal(null, err);
+        console.log("Connected correctly to server.");
+
+         db.collection(q).find({ },{ _id: 0,  __v : 0} ).toArray(function (err, result) {
+            if (err) throw err;
+             data=result;
+             res.send(result);
+             console.log(result);
+             console.log(typeof result);
+        });
+   //     res.send(data);
+        db.close();
+    });
+
 });
 
 module.exports = router;
